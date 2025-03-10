3531d2abe0ff8ea4b7556741309980940580cc07
mimilopezpalaci <miriamlopezpalacios3@gmail.com>
2025-03-10 08:59:57 +0100

    ejer

---
+++ script.js
@@ -0,0 +1,7 @@
+
+        $(document).ready(function() {
+            // Al hacer clic en el botón, ocultar el párrafo
+            $("#ocultar-btn").click(function() {
+                $("#parrafo").hide(); // Usar el método hide() para ocultar el párrafo
+            });
+        });
